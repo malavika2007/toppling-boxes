948e5e7287c9ae6886223e9a6fa6bf5d479010c1
malavika2007 <85999768+malavika2007@users.noreply.github.com>
2021-07-12 16:31:32 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,23 @@
+class ground{
+    constructor(x,y,width,height)
+    {
+        var option={
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,width,height,option);
+        World.add(world,this.body);
+        
+        this.width=width;
+        this.height=height;
+    }
+
+    display()
+    {
+        var pos=this.body.position;
+        rectMode(CENTER);
+        fill("green")
+        rect(pos.x,pos.y,this.width,this.height);    
+    }
+
+
+}
